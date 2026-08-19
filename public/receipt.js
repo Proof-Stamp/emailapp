0b01d00128dcad2a04c5f66ea286e763618c9d1c
Sasha <sasha@techbridge.ca>
2026-08-19 15:23:25 -0400

    Bump receipt app version to 0.3.3

--- public/receipt.js
+++ public/receipt.js
@@ -3,7 +3,7 @@ import { trackEmailAppOpened, trackProofCreated } from './metrics.js'
 
 export const RECEIPT_SCHEMA = 'org.proofstamp.email-receipt'
 export const RECEIPT_VERSION = '2.0'
-export const APP_VERSION = '0.3.2'
+export const APP_VERSION = '0.3.3'
 export const VERIFICATION_URL = 'https://email.proofstamp.org/verify'
 export const CREATE_URL = 'https://email.proofstamp.org/'
 
