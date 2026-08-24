f5627bb4529d114ee77514ffe1daafb2c28b1d60
Sasha <sasha@techbridge.ca>
2026-08-24 13:37:41 -0400

    refactor: share hash disagreement guard

--- public/dual-hash.js
+++ public/dual-hash.js
@@ -1,17 +1,6 @@
 import { sha256Bytes } from './hash.js'
+import { assertHashesAgree } from './hash-agreement.js'
 import { rustSha256Bytes } from './rust-sha256.js'
-
-export class HashEngineDisagreementError extends Error {
-  constructor() {
-    super('Local SHA-256 implementations disagreed')
-    this.name = 'HashEngineDisagreementError'
-  }
-}
-
-export function assertHashesAgree(first, second) {
-  if (first !== second) throw new HashEngineDisagreementError()
-  return first
-}
 
 export async function dualSha256Bytes(bytes) {
   const input = bytes instanceof Uint8Array ? bytes : new Uint8Array(bytes)
